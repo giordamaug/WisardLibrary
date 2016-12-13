29c27684e1671517603e7f7d17329bd12ad78f0d
Maurizio Giordano <maurizio.giordano@cnr.it>
2016-12-13 15:51:06 +0100

    new files

---
+++ wisard.py
@@ -0,0 +1,176 @@
+"""
+    WiSARD Python Class
+    
+    Created by Maurizio Giordano on 13/12/2016
+    
+"""
+
+import wisardwrapper as wisapi
+import numpy as np
+
+class WiSARD:
+
+    def __init__(self,
+                 nobits,
+                 bleaching=True,
+                 default_bleaching=1,
+                 confidence_bleaching=0.1,
+                 randomized=True,
+                 seed=424242):
+        if (not isinstance(nobits, int)):
+            raise Exception('number of bits must be an integer')
+        if (not isinstance(bleaching, bool)):
+            raise Exception('bleaching flag must be a boolean')
+        if (not isinstance(default_bleaching, int)):
+            raise Exception('bleaching downstep must be an integer')
+        if (not isinstance(randomized, bool)):
+            raise Exception('randomization flag must be a boolean')
+        if (not isinstance(seed, int)):
+            raise Exception('random seed must be an integer')
+        if (not isinstance(confidence_bleaching, float)):
+            raise Exception('bleaching confidence must be a float between 0 and 1')
+        self._nobits = nobits
+        self._bleaching = bleaching
+        self._def_bleaching = default_bleaching
+        self._conf_bleaching = confidence_bleaching
+        self._randomized = randomized
+        self._seed = seed
+
+        # attributes set after initialization
+        self._retina_size = 0
+        self._norams_x_discr = 0
+        self._discriminators = {}
+        self._classes = []
+
+    def getNoBits(self):
+        return self._nobits
+
+    def getSize(self):
+        return self._retina_size
+
+    def getNoRams(self):
+        return self._discriminators * self._norams_x_discr
+
+    def getDiscr(self,name):
+        return self._discriminators[name]
+
+    def isBleachingSet(self):
+        return self._bleaching
+
+    def setBleaching(self):
+        self._bleaching = True
+
+    def unsetBleaching(self):
+        self._bleaching = False
+
+    def printDiscr(self,name):
+        return wisapi.printDiscr(self._discriminators[name])
+    
+    # make tuple input for discriminator (from array of bits)
+    def _mkTuple(self,discr,sample):
+        if (isinstance(sample, list)):
+            sample = np.array(sample)
+        if (isinstance(sample,np.ndarray)) and (len(sample.shape) == 1):
+            raw_sample = sample
+        else:
+            raise Exception("sample must be flat list (or array) of integers: %s"%sample)
+            
+        intuple = [0 for i in range(self._norams_x_discr)]
+        for i in range(self._norams_x_discr):
+            for index in range(self._nobits):
+                x = wisapi.getMapRefDiscr(discr, (i * self._nobits) + index) % self._retina_size
+                intuple[i] += (2**(self._nobits -1 - index)) * sample[x]
+        return intuple
+            
+    # create and train wisard on labeled dataset X,y
+    # X is a matrix of retinas (each line will be a retina)
+    # y is a list of label (each line defines a retina in the same position in Y)
+    def fit(self, X, y):
+        # creating discriminators
+        self._retina_size = len(X[0])
+        classes = set(y)
+        
+        for clname in y:
+            # use WiSARD C wrapper
+            d = wisapi.makeDiscr(self._nobits,
+                                 self._retina_size,
+                                 name=str(clname),
+                                 maptype = 'random' if self._randomized else 'linear')
+                              
+            self._discriminators[str(clname)] = d
+        
+        self._norams_x_discr = wisapi.getNRamDiscr(self._discriminators[self._discriminators.keys()[0]])
+        self._classes = self._discriminators.keys()
+        
+        # add training
+        num_samples = len(y)
+        for i in xrange(num_samples):
+            retina = X[i]
+            label = y[i]
+            
+            # make tuple from array of bits
+            tuple = self._mkTuple(self._discriminators[label],retina)
+            wisapi.trainDiscr(self._discriminators[label],tuple)
+
+    #  X is a list of of lists of retinas (or a 2-dim array of retinas)
+    def predict(self, X):
+        final_result = []
+        X = np.array(X)  # convert to numpy matrix
+        results = self.predict_proba(X)
+        for res in results:
+            index = np.argmax(res)
+            final_result.append(self._classes[index])
+        
+        return final_result
+
+    def predict_proba(self, X):
+        result = []
+        if self._bleaching:
+            return np.apply_along_axis(self._predict_bleaching,1,X)
+        else:
+            return np.apply_along_axis(self._predict_no_bleaching,1,X)
+
+    def _predict_no_bleaching(self, x):
+        return np.array([wisapi.classifyDiscr(self._discriminators[class_name],self._mkTuple(self._discriminators[class_name],x)) for class_name in self._classes])
+
+    def _predict_bleaching(self, x):
+        b = self._def_bleaching
+        confidence = 0.0
+        result_partial = None
+        
+        res_disc = np.array([wisapi.responseDiscr(self._discriminators[class_name],self._mkTuple(self._discriminators[class_name],x)) for class_name in self._classes])
+        
+        print res_disc
+        while confidence < self._conf_bleaching:
+            result_partial = np.sum(res_disc >= b, axis=1)
+            confidence = self._calc_confidence(result_partial)
+            b += 1
+            print result_partial, b, confidence
+            if(np.sum(result_partial) == 0):
+                result_partial = np.sum(res_disc >= 1, axis=1)
+                break
+        result_sum = np.sum(result_partial, dtype=np.float32)
+        result = np.array(result_partial)/result_sum
+        
+        return result
+
+    def _calc_confidence(self, results):
+        # getting max value
+        max_value = results.max()
+        if(max_value == 0):
+            return 0
+
+        # if there are two positions with same value
+        position = np.where(results == max_value)
+        if position[0].shape[0]>1:
+            return 0
+
+        # getting second max value
+        second_max = results[results < max_value].max()
+        if results[results < max_value].size > 0:
+            second_max = results[results < max_value].max()
+        
+        # calculating confidence value
+        c = 1 - float(second_max) / float(max_value)
+        
+        return c
---
+++ wisardwrapper.py
@@ -0,0 +1,271 @@
+"""
+    WiSARD C Library Wrapper in Python
+    
+    Created by Maurizio Giordano on 13/12/2016
+    
+"""
+
+from ctypes import *
+import os
+import random
+import platform
+if platform.system() == 'Linux':
+    suffix = '.so'
+elif platform.system() == 'Windows':
+    suffix = '.dll'
+elif platform.system() == 'Darwin':
+    suffix = '.dylib'
+else:
+    raise Error("Unsupported Platform")
+
+libpath = "libwisard-cxx_3.0" + suffix
+wizl = CDLL(os.path.join(os.environ['PWD'], libpath))
+HEADKEY = 18446744073709551615L
+
+""" Mapping data structure """
+
+c_value = c_double
+c_key = c_ulong
+
+class Wentry(Structure):
+    pass
+
+class Wkeymax(Structure):
+    pass
+
+Wkeymax._fields_ = [("key", c_key),
+                    ("value", c_value)]
+
+Wentry._fields_ = [("key", c_key),
+                   ("value", c_value),
+                   ("next", POINTER(Wentry)),
+                   ("prev", POINTER(Wentry))]
+
+class Discr(Structure):
+    _fields_ = [("n_ram", c_int),
+                ("n_bit", c_int),
+                ("n_loc", c_key),
+                ("size", c_int),
+                ("tcounter", c_ulong),
+                ("rams", POINTER(POINTER(Wentry))),
+                ("maxkeys", POINTER(Wkeymax)),
+                ("map", POINTER(c_int)),
+                ("rmap", POINTER(c_int)),
+                ("mi", POINTER(c_value)),
+                ("maxmi", c_value),
+                ("name", c_char_p)]
+
+def discr_n_ram(discr):
+    return discr.contents.n_ram
+
+""" RAM interface """
+_wram_create = wizl.wram_create
+_wram_create.restype =  POINTER(Wentry)
+_wram_set = wizl.wram_set
+_wram_set.argtypes = [ POINTER(Wentry), c_key, c_value ]
+_wram_set.restype =  c_value
+_wram_incr = wizl.wram_incr
+_wram_incr.argtypes = [ POINTER(Wentry), c_key ]
+_wram_incr.restype =  c_value
+_wram_decr = wizl.wram_decr
+_wram_decr.argtypes = [ POINTER(Wentry), c_key ]
+_wram_decr.restype =  c_value
+_wram_decr_all_but_key = wizl.wram_decr_all_but_key
+_wram_decr_all_but_key.argtypes = [ POINTER(Wentry), c_key ]
+_wram_decr_all_but_key.restype =  c_value
+_wram_del = wizl.wram_del
+_wram_del.argtypes = [ POINTER(Wentry), c_key ]
+_wram_del.restype =  c_value
+_wram_get = wizl.wram_get
+_wram_get.argtypes = [ POINTER(Wentry), c_key ]
+_wram_get.restype =  c_value
+_wram_len = wizl.wram_len
+_wram_len.argtypes = [ POINTER(Wentry) ]
+_wram_len.restype =  c_int
+
+def wram_build(n,dict):
+    ram = _wram_create()
+    maxkey = 2**n
+    for key,value in dict.items():
+        if key > 0:
+            _wram_set(ram,key,value)
+    return ram
+
+def wram_get(ram, key):
+    return _wram_get(ram,c_key(key))
+
+def wram_incr(ram, key):
+    return _wram_incr(ram,c_key(key))
+
+def wram_decr(ram, key):
+    return _wram_decr(ram,c_key(key))
+
+def wram_decr_all_but_key(ram,key):
+    return _wram_decr_all_but_key(ram,c_key(key))
+
+def wram_del(ram, key):
+    return _wram_del(ram,c_key(key))
+
+def wram_set(ram, key, value):
+    return _wram_set(ram,c_key(key),c_value(value))
+
+def showRam(ram):
+    size = _wram_len(ram)
+    p = ram.contents.next
+    res = {}
+    while p.contents.key != HEADKEY:
+        res[int(p.contents.key)] = p.contents.value
+        p = p.contents.next
+    return res
+
+""" Constructor interface """
+_makeDiscr =  wizl.makeDiscr
+_makeDiscr.restype =  POINTER(Discr)
+_makeDiscr.argtypes = [ c_int, c_int, c_char_p, c_char_p ]
+
+def makeDiscr(nbit, size, name='anonym', maptype='random'):
+    return _makeDiscr(c_int(nbit), c_int(size), c_char_p(name), c_char_p(maptype))
+
+""" Train/Classify wrappers"""
+_trainDiscr = wizl.trainDiscr
+_trainDiscr.argtypes = [ POINTER(Discr), POINTER(c_key) ]
+_trainforgetDiscr = wizl.trainforgetDiscr
+_trainforgetDiscr.argtypes = [ POINTER(Discr), POINTER(c_key) , c_value, c_value]
+_classifyDiscr = wizl.classifyDiscr
+_classifyDiscr.argtypes = [ POINTER(Discr), POINTER(c_key) ]
+_classifyDiscr.restype = c_double
+_classifyDiscrThresholded = wizl.classifyDiscrThresholded
+_classifyDiscrThresholded.argtypes = [ POINTER(Discr), POINTER(c_key), c_value ]
+_classifyDiscrThresholded.restype = c_double
+
+_responseDiscr = wizl.responseDiscr
+_responseDiscr.argtypes = [ POINTER(Discr), POINTER(c_key) ]
+_responseDiscr.restype = POINTER(c_double)
+
+def trainDiscr(discr, intuple):
+    n = len(intuple)
+    if n != discr.contents.n_ram:
+        raise NameError('Wrong InTuple size')
+    c_tuple = (c_ulong * n)(*intuple)
+    _trainDiscr(discr, c_tuple)
+
+def trainforgetDiscr(discr, intuple, incr, decr):
+    n = len(intuple)
+    if n != discr.contents.n_ram:
+        raise NameError('Wrong InTuple size')
+    c_tuple = (c_ulong * n)(*intuple)
+    _trainforgetDiscr(discr, c_tuple, incr, decr)
+
+def classifyDiscr(discr, intuple):
+    n = len(intuple)
+    if n != discr.contents.n_ram:
+        raise NameError('Wrong InTuple size')
+    c_tuple = (c_ulong * n)(*intuple)
+    return _classifyDiscr(discr, c_tuple)
+
+def classifyDiscrThresholded(discr, intuple, threshold):
+    n = len(intuple)
+    if n != discr.contents.n_ram:
+        raise NameError('Wrong InTuple size')
+    c_tuple = (c_ulong * n)(*intuple)
+    return _classifyDiscrThresholded(discr, c_tuple, threshold)
+
+def responseDiscr(discr, intuple):
+    n = len(intuple)
+    if n != discr.contents.n_ram:
+        raise NameError('Wrong InTuple size')
+    rawres = _responseDiscr(discr, (c_ulong * n)(*intuple))
+    return [ rawres[i] for i in range(discr.contents.n_ram)]
+
+
+""" Mental Image methods """
+_mentalDiscr = wizl.mentalDiscr
+_mentalDiscr.argtypes = [ POINTER(Discr) ]
+_mentalDiscr.restype = c_value
+
+def mentalDiscr(discr):
+    return _mentalDiscr(discr)
+
+""" Utilities wrapper """
+_printDiscr =  wizl.printDiscr
+
+def printDiscr(discr):
+    _printDiscr(discr)
+
+def getNBitDiscr(discr):
+    return discr.contents.n_bit
+
+def getNRamDiscr(discr):
+    return discr.contents.n_ram
+
+def getNameDiscr(discr):
+    if not discr.contents.name:
+        return ''
+    return discr.contents.name
+
+def getTcounterDiscr(discr):
+    return discr.contents.tcounter
+
+def getMapRefDiscr(discr, index):
+    return discr.contents.map[index]
+
+def getRMapRefDiscr(discr, index):
+    return discr.contents.rmap[index]
+
+def getSizeDiscr(discr):
+    return discr.contents.size
+
+def getMaxMIDiscr(discr):
+    return discr.contents.maxmi
+
+def getMIRefDiscr(discr,index):
+    return discr.contents.mi[index]
+
+def getMapDiscr(discr):
+    if not discr.contents.map:
+        return []
+    res = [ None for i in range(discr.contents.size)]
+    for i in range(discr.contents.size):
+        res[i] = discr.contents.map[i]
+    return res
+
+def setMapDiscr(discr,map):
+    if discr.contents.size != len(map):
+        raise Error("Error: input map has wrong size")
+    for i in range(discr.contents.size):
+        discr.contents.map[i] = map[i]
+        discr.contents.rmap[map[i]] = i
+    return
+
+def getRMapDiscr(discr):
+    if not discr.contents.rmap:
+        return []
+    res = [ None for i in range(discr.contents.size)]
+    for i in range(discr.contents.size):
+        res[i] = discr.contents.rmap[i]
+    return res
+
+def getMaxKeysDiscr(discr):
+    if not discr.contents.maxkeys:
+        return []
+    return [ (discr.contents.maxkeys[i].key,discr.contents.maxkeys[i].value) for i in range(discr.contents.n_ram)]
+
+def getMIDiscr(discr):
+    if not discr.contents.mi:
+        return []
+    res = [ None for i in range(discr.contents.size)]
+    for i in range(discr.contents.size):
+        res[i] = discr.contents.mi[i]
+    return res
+
+def getRamsDiscr(discr):
+    res = [ None for i in range(discr.contents.n_ram)]
+    for i in range(discr.contents.n_ram):
+        ram = discr.contents.rams[i]
+        p = ram.contents.next
+        res[i] = {}
+        while p.contents.key != HEADKEY:
+            res[i][int(p.contents.key)] = p.contents.value
+            p = p.contents.next
+    return res
+
